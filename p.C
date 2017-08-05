a7769caf1f84c78286738b2c40aebf20a1c90326
sakthisanjay96 <30748370+sakthisanjay96@users.noreply.github.com>
2017-08-05 16:24:25 +0530

    p.C
    this is a program

---
+++ p.C
@@ -0,0 +1,13 @@
+#inlude<stdio.h>
+void main()
+{
+int num;
+printf("enter the number:\n);
+scanf("%d:,&num);
+if(num>0)
+printf("%d is a positive number  \n",num);
+else if(num<0)
+printf("%d is a negative number \n",num);
+else
+printf("0 is neither positive nor negative");
+}
